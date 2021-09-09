9048aab798b7cd9b158855a95d03eb968a84fb24
Feroz Mohammad <mohammadferoz44@gmail.com>
2021-09-09 11:54:33 +0530

    books pagination

--- application/client/views/home.php
+++ application/client/views/home.php
@@ -25,13 +25,13 @@
 <!-- </div>
   </div> -->
 
-<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
-<script type="text/javascript">
+<!-- <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script> -->
+<!-- <script type="text/javascript">
     $(function() {
         paginate();
 
         function paginate() {
-            $('#ajax_links a').click(function() {
+            $('#abc a').click(function() {
                 var url = $(this).attr('href');
                 $.ajax({
                     url: url,
@@ -45,4 +45,4 @@
             });
         }
     });
-</script>
+</script> -->
