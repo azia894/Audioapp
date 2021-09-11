7688307c61bce51f49666a8116b98ba274b92ae5
Feroz Mohammad <mohammadferoz44@gmail.com>
2021-09-11 20:56:06 +0530

    chapter view changed

--- application/client/views/view.php
+++ application/client/views/view.php
@@ -13,67 +13,63 @@
                 <p class="book-page-author">
                     <a href="<?= base_url('authors/view/' . $record['id']) ?>">
                         <?= ucwords($record['aut_name']) ?>
-                        <!-- <span class="dod-dob">(<?= $record['dob'] ?>)</span> -->
+                        <!-- <span class="dod-dob">(<?= $record['dob'] ?>)</span>
+                        -->
                     </a>
                 </p>
-                <p class="book-meta">Published In: <?= $record['bk_year'] ?> </p>
+                <p class="book-meta">Published In: <?= $record['bk_year'] ?>
+                </p>
                 <p class="description"><br /><?= $record['bk_desc'] ?><br /><br />
                 </p>
                 <!-- <p class="book-page-genre"><span>Genre(s):</span> Action & Adventure</p> -->
                 <!-- <p class="book-page-genre"><span>Language:</span> English</p> -->
             </div> <!-- end .content-wrap -->
-            <table class="chapter-download">
-                <thead>
-                    <tr>
-                        <th>ID </th>
-                        <th>Audio </th>
-                        <th>Chapter</th>
-                        <th>Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    if ($getdata == null) {
-                    ?>
+            <div class="table-responsive">
+                <table class="chapter-download">
+                    <thead>
                         <tr>
-                            <td colspan="7">No Data to display</td>
-                        <tr>
-                            <?php
-                        } else {
-                            $i = 1;
-                            foreach ($getdata as $row) {
-                            ?>
-                        <tr>
-                            <td style="vertical-align: middle;"><?php echo $i; ?>
-                            </td>
-                            <td><audio controls>
-                                    <source src="<?= $row->ch_audio ?>" type="audio/ogg">
-                                    <source src="<?= $row->ch_audio ?>" type="audio/mpeg">
-                                    Your browser does not support the audio element.
-                                </audio></td>
-                            <td style="vertical-align: middle;">
-                                <span class="chapter-name">
-                                    <?php echo $row->ch_name ?>
-                                </span>
-                            </td>
-                            <td style="vertical-align: middle;"><?php echo $row->ch_duration ?>
-                            </td>
+                            <th>ID </th>
+                            <th>Audio </th>
+                            <th>Chapter</th>
+                            <th>Time</th>
                         </tr>
-                <?php
-                                $i++;
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($getdata == null) {
+                        ?>
+                            <tr>
+                                <td colspan="7">No Data to display</td>
+                            <tr>
+                                <?php
+                            } else {
+                                $i = 1;
+                                foreach ($getdata as $row) {
+                                ?>
+                                    <tr>
+                                        <td style="vertical-align: middle;"><?php echo $i; ?>
+                                        </td>
+                                        <td><audio controls>
+                                                <source src="<?= $row->ch_audio ?>" type="audio/ogg">
+                                                <source src="<?= $row->ch_audio ?>" type="audio/mpeg">
+                                                Your browser does not support the audio element.
+                                            </audio></td>
+                                        <td style="vertical-align: middle;">
+                                            <span class="chapter-name">
+                                                <?php echo $row->ch_name ?>
+                                            </span>
+                                        </td>
+                                        <td style="vertical-align: middle;"><?php echo $row->ch_duration ?>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    $i++;
+                                }
                             }
-                        }
-                ?>
-                </tbody>
-            </table>
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div><!-- end .page -->
     </div>
 </div>
-<!-- </div>
-</div>
-</div>
-</div>
-</div>
-</section>
-</div>
-</div> -->
