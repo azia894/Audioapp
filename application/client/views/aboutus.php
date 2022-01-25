4317b1493331b39c8cacd95be30b85854ec148dd
Feroz Mohammad <mohammadferoz44@gmail.com>
2022-01-25 15:22:47 +0530

    about us page changes

--- application/client/views/aboutus.php
+++ application/client/views/aboutus.php
@@ -2,125 +2,132 @@
   <div class="tab-content">
     <h3>About Us</h3>
     <div class="container">
-      <div>
-        <div>
-          <h2><b>Aim</b> </h2>
-          <p>Tap into the wisdom and tradition written in Urdu&nbsp;and preserve the language.</p>
-          <h2><b>Objective</b></h2>
-          <p>To create a <strong>mobile app</strong> and an <strong>online portal</strong> where people can listen to
-            Urdu Audiobooks for free.</p>
-          <h2><b>Background</b></h2>
-          <p>Information can now be accessed instantaneously at the point of need. The ease of recording and retrieving
-            high quality audio has made podcasts and audiobooks attractive new methods of learning. Audiobooks in
-            English is already a big industry. Long commutes provide a perfect opportunity to listen to audiobooks on
-            handheld mobile devices.&nbsp;</p>
-          <p>There is a large cohort of Urdu speaking people around the world who lack the necessary reading skills to
-            benefit from the ocean of knowledge, wisdom and tradition preserved in Urdu literature. Through this project
-            we hope to make Urdu non-fiction and fiction easily accessible in the form of audiobooks for adults and
-            children.&nbsp;</p>
-          <section id="supportus">
+      <h2><b>Aim</b> </h2>
+      <p>Tap into the wisdom and tradition written in Urdu&nbsp;and preserve the language.</p>
+      <h2><b>Objective</b></h2>
+      <p>To create a <strong>mobile app</strong> and an <strong>online portal</strong> where people can listen to
+        Urdu Audiobooks for free.</p>
+      <h2><b>Background</b></h2>
+      <p>Information can now be accessed instantaneously at the point of need. The ease of recording and retrieving
+        high quality audio has made podcasts and audiobooks attractive new methods of learning. Audiobooks in
+        English is already a big industry. Long commutes provide a perfect opportunity to listen to audiobooks on
+        handheld mobile devices.&nbsp;</p>
+      <p>There is a large cohort of Urdu speaking people around the world who lack the necessary reading skills to
+        benefit from the ocean of knowledge, wisdom and tradition preserved in Urdu literature. Through this project
+        we hope to make Urdu non-fiction and fiction easily accessible in the form of audiobooks for adults and
+        children.&nbsp;</p>
+      <section id="supportus">
 
-            <h2><b>Support us</b></h2>
-            <!-- <p>At https://maahiruk.org/support-us/</p> -->
+        <h2><b>Support us</b></h2>
+        <!-- <p>At https://maahiruk.org/support-us/</p> -->
 
-            <p> You can support us in the following ways:</p>
+        <p> You can support us in the following ways:</p>
 
-            <p> Become a part of the <b>admin / technical team.</b> Basic IT and media skills will come handy.</p>
+        <p> Become a part of the <b>admin / technical team.</b> Basic IT and media skills will come handy.</p>
 
-            <p>Become a <b>narrator</b> if you enjoy reading Urdu books. Send in a 5 minutes sample recording of your reading from a book of your liking to urduabproject@gmail.com. You can use a simple PC or mobile phone voice recording app. Our team of experts will feedback.</p>
+        <p>Become a <b>narrator</b> if you enjoy reading Urdu books. Send in a 5 minutes sample recording of your reading from a book of your liking to urduabproject@gmail.com. You can use a simple PC or mobile phone voice recording app. Our team of experts will feedback.</p>
 
-            <p>Suggest a <b>book</b>. See under book suggestions guidelines.</p>
+        <p>Suggest a <b>book</b>. See under book suggestions guidelines.</p>
 
-            <p><b>Donate</b>. Dil ki Awaz Urdu Audiobooks is a not for profit project sponsored by Urdu loving individuals and charitable organisations. Many of our team members have joined on a voluntary basis. However we would like to pay the narrators a small token of appreciation for their talent and effort. The pay rate would depend on the individual narrators. Since the project is charitably funded we would like to support those narrators with low salaries at their primary workplaces. Our second expenditure is for building, maintaining and enhancing our mobile App. We look forward to any support we can receive from our well-wishers, subscribers and lovers of Urdu.</p>
+        <p><b>Donate</b>. Dil ki Awaz Urdu Audiobooks is a not for profit project sponsored by Urdu loving individuals and charitable organisations. Many of our team members have joined on a voluntary basis. However we would like to pay the narrators a small token of appreciation for their talent and effort. The pay rate would depend on the individual narrators. Since the project is charitably funded we would like to support those narrators with low salaries at their primary workplaces. Our second expenditure is for building, maintaining and enhancing our mobile App. We look forward to any support we can receive from our well-wishers, subscribers and lovers of Urdu.</p>
 
-            <p>https://forms.gle/C5C1qzsmPaKV6oAi6 complete this form for becoming a part of the project</p>
+        <p><a href="https://forms.gle/C5C1qzsmPaKV6oAi6" style="text-decoration: underline;">https://forms.gle/C5C1qzsmPaKV6oAi6</a> complete this form for becoming a part of the project</p>
 
-          </section>
-          <!-- <p>
+      </section>
+      <!-- <p>
             <a href="https://maahiruk.org/support-us/" target="_blank">https://maahiruk.org/support-us/</a>
           </p> -->
-          <h2><b>Our Team</b></h2>
-          <div class="row">
-            <div class="col-md-4">
-              <h1>Advisory Group</h1>
-              <ul>
-                <li>Shortlist works to be recorded in audio within each of the genres</li>
-                <li>General advice and direction</li>
-                <li>Promote this project</li>
-              </ul>
-            </div>
-            <div class="col-md-4">
-              <h1>Narrators</h1>
-              <ul>
-                <li>Give audition</li>
-                <li>Record part or a section of a book</li>
-              </ul>
-            </div>
-            <div class="col-md-4">
-              <h1>Auditioners</h1>
-              <ul>
-                <li>Allocate narrators to appropriate Urdu works</li>
-                <li>Proof read</li>
-              </ul>
-            </div>
+      <h2><b>Our Team</b></h2>
+      <div class="row">
+        <div class="col-md-4">
+          <h1>Advisory Group</h1>
+          <ul>
+            <li>Shortlist works to be recorded in audio within each of the genres</li>
+            <li>General advice and direction</li>
+            <li>Promote this project</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h1>Narrators</h1>
+          <ul>
+            <li>Give audition</li>
+            <li>Record part or a section of a book</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h1>Auditioners</h1>
+          <ul>
+            <li>Allocate narrators to appropriate Urdu works</li>
+            <li>Proof read</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Sponsors</h2>
+        </div>
+        <div id="container">
+          <h3><a href="https://maahiruk.org/" target="_blank">MAAHIR</a></h3>
+          <h3>Urdu Lovers (YOU!)</h3>
+          <div style="vertical-align: middle;">
+            <img src="<?php echo base_url() . 'assets/images/maahir.png'; ?>" alt="maahiruk.org" class="" style="width: 200px;">
+            <img src="<?php echo base_url() . 'assets/images/daana.jpg'; ?>" alt="deccan alumni association" class="" style="width: 200px;">
           </div>
-          <div>
-            <div>
-              <h2>Sponsors</h2>
-            </div>
-            <div id="container">
-              <h3><a href="https://maahiruk.org/" target="_blank">MAAHIR</a></h3>
-              <h3>Urdu Lovers (YOU!)</h3>
-              <!-- <div class="description">
-                <p>
-                  MAAHIR is an organisation of professionals from Hyderabad, Deccan and its surrounding regions (the
-                  erstwhile Nizam state) who have settled in the United Kingdom but retain a strong emotional and
-                  cultural bond with Hyderabad. The idea was conceived in discussions over a short period of time by the
-                  core group members who realised that migrants from Hyderabad were all highly educated and well
-                  established in various professional fields and were spread across Great Britain. For such a group to
-                  come together on one platform would be really useful as the combined resources would benefit people in
-                  Hyderabad and also new migrants looking to settle in the United Kingdom. The candles that we hope to
-                  light will illuminate the path for the generations to follow and represent our attempt at giving back
-                  to society what we have gained through a kind providence and through the help that we received from
-                  our own benefactors.
-                </p>
-                <p>
-                  To our delight from the first discussions to actually launching the group was but a short step and
-                  over the first month since the original idea was floated we had a group of 150 dedicated professionals
-                  from Hyderabad with a wide representation of specialties (IT professionals, Doctors, Academics,
-                  Finance Managers, Architects, and Immigration Consultants… the list keeps growing).
-                </p>
-                <p>
-                  Over the following month we increased our membership with students now joining the group as the
-                  realisation grew that in MAAHIR we had a group of mentors who were eager to share their knowledge and
-                  experience to help students with career choices, job interview skills and CV building exercises. Our
-                  other objectives are focussed on improving educational opportunities to poor children in Hyderabad and
-                  in improving healthcare facilities and also providing increased employment opportunities for women in
-                  Hyderabad by ensuring easy acquisition of vocational educational skills.
-                </p>
-                <p>
-                  We are now a fully registered UK charity. We have a good organisational structure in place. Our first
-                  project, chosen from a selection of proposals from our members, are closely aligned to our objectives
-                  of improving educational opportunities for poor students in Hyderabad. Our second project tackles the
-                  dangers of fake news and misinformation as we are all increasingly aware of the importance of the
-                  power of social media in shaping attitudes and opinions of people of all ages. In addition to this, as
-                  a group of concerned Hyderabadis we are also keen on responding to situations in Hyderabad that
-                  require charity funds like the lockdown in the COVID pandemic and more recently the devastating floods
-                  that have caused havoc in Hyderabad.
-                </p>
-                <p>
-                  We look forward to exciting and challenging – but also rewarding times ahead. Our talented group
-                  members are our strength- and we are confident that we can do a lot of good work in the future,
-                  InshaAllah. Please keep visiting this page to learn about new developments.
-                </p>
-                <p>
-                  <a href="https://maahiruk.org" target="_blank">https://maahiruk.org/</a>
-                </p>
-              </div> -->
-            </div>
-          </div>
+        </div>
+        <hr />
+        <div>
+          <h1><b>Book suggestions</b></h1>
+          <p>Any well known and well accepted work written or translated into Urdu may be suggested. </p>
+          <p>The work may belong to any genre &#8211; typically short stories, society, religion, children, adventure, politics, humour, History, biography, novel, fiction, philosophy etc&#8230; These categories are not exhaustive.</p>
+          <p>No book that has obscene or vulgar references shall be included however acclaimed work it may be.</p>
+          <p>Any one from the project team, app users or general public can suggest a book. The project management team will prioritise the suggestions depending on the user demand and availability of narrators pool. </p>
+          <h2><b>Guidance for the Narrators</b></h2>
+          <ul style="list-style-type: circle;">
+            <li>Record each chapter in a separate audio file. The file size will therefore depend on the chapter size.</li>
+            <li>Please include foreword, preface, introduction of the book/author etc (harf aghaz, pesh lafz, muqaddimah) in your narration</li>
+            <li>Please include in a separate audio file list of hard words with meanings in Urdu. </li>
+            <li>Towards the end of each audio file include the following tag line [<em>to be confirmed…..!!!Dil ki Awaz Urdu Audiobooks ke liye aap sunrahe thhe ……!!!</em>]</li>
+            <li>Do not add any background music or modifications like echo or reverb. Editing to improve voice quality are okay.</li>
+            <li>If you come across any obscene or vulgar lines in the book please report to the project manager and hold off narrating the book. These should have been ideally detected at the time of book selection.</li>
+            <li>Narrators should be very careful in reading the text accurately without adding or removing anything. Our patrons have advised that narrators should consider their work as an <em>amaanat </em>(a trust) whilst completing the task.</li>
+          </ul>
+          <p><strong>Technical guidance</strong></p>
+          <ul style="list-style-type: circle;">
+            <li>Modern smart phones have excellent quality voice recording capability. A simple collar microphone kept at a fixed distance is often all you need.&nbsp;</li>
+            <li>Inbuilt mobile phone apps also have the function of moving back a segment and rerecording if there is a mistake.</li>
+            <li>Make sure in the settings the recording is stereophonic not monophonic. The default setting is usually stereophonic.</li>
+            <li>Using a profession studio facility for recording is not necessary but does have advantages.</li>
+            <li>Regardless of the technical set up you are using please be sure to send a 5 minutes sample of the audio to the project manager before starting the main work.</li>
+          </ul>
+          <p><strong>Essential recording tips by Sajida sahiba</strong></p>
+          <ul style="list-style-type: circle;">
+            <li>Print the reading material</li>
+            <li>Have a cup of water handy</li>
+            <li>Keep the device about 1.5 feet away from your mouth to avoid breathing into it, so the voice is not too loud or too quiet</li>
+            <li>Be careful with the sounds of ruffling paper</li>
+            <li>Pause to take a breath and swallow</li>
+            <li>Maintain the enthusiasm. Loss of it will easily show in your voice quality.</li>
+            <li>Do take breaks</li>
+            <li>Compatibility of the app &#8211; convert to the audio &#8211;</li>
+            <li>Be aware of local dialects/ talaffuz / accent</li>
+            <li>Rehearse</li>
+            <li>Listen to your own voice</li>
+            <li>Speak in a natural way, emotions &#8211; sadness happiness</li>
+            <li>Feeling the words in the text while you read. Know the story.</li>
+            <li>Make it your Dil ki Awaz</li>
+            <li>Give natural and human touch</li>
+            <li>Become the character you are narrating</li>
+            <li>Exercise vocal muscles to relax, breathing exercises on the internet.</li>
+            <li>Clear pronunciation</li>
+            <li>Gestures are important even though you are only giving your voice. Even if you are alone</li>
+            <li>Be confident</li>
+            <li>Narrate the story like your own story.</li>
+            <li>Early morning voice is most clear</li>
+            <li>Look after your voice</li>
+          </ul>
+          <figure class="wp-block-image size-large"><img loading="lazy" width="709" height="1024" src="<?php echo base_url() . 'assets/images/aboutusbanner.jpg'; ?>" alt="" class="wp-image-1884" srcset="https://maahiruk.org/wp-content/uploads/2022/01/newposter-709x1024.jpg 709w, https://maahiruk.org/wp-content/uploads/2022/01/newposter-208x300.jpg 208w, https://maahiruk.org/wp-content/uploads/2022/01/newposter-242x350.jpg 242w, https://maahiruk.org/wp-content/uploads/2022/01/newposter-660x953.jpg 660w, https://maahiruk.org/wp-content/uploads/2022/01/newposter.jpg 720w" sizes="(max-width: 709px) 100vw, 709px" /></figure>
+
         </div>
       </div>
     </div>
   </div>
-</div>
